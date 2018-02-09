5e78f31ce661ecf6ffa562b9275272474f7995a3
Rafael Vidaurre <narzerus@gmail.com>
2018-02-09 14:12:44 -0300

    correct absolute path resolving

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './base.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import 'base.css';
+import App from 'components/App';
+import registerServiceWorker from 'registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
